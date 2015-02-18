5d18ce0f980da9f3323e12a009c6e8e88e255a45
Gabriel <gabrielrcouto@gmail.com>
2015-02-18 19:55:44 -0200

    First Commit Passando ainda para composer e aplicando testes

--- test.php
+++ test.php
@@ -3,8 +3,8 @@ require 'metaphone.php';
 
 use Metaphone\Metaphone;
 
-echo Metaphone::getPhraseMetaphone('é um teste super legal') . PHP_EOL;
-echo Metaphone::getPhraseMetaphone('chocolate e farinha') . PHP_EOL;
+echo Metaphone::getPhraseMetaphone('é um testi super legal') . PHP_EOL;
+echo Metaphone::getPhraseMetaphone('chocolate e farinha faquir') . PHP_EOL;
 echo Metaphone::getPhraseMetaphone('acerola asserola aserola') . PHP_EOL;
 echo Metaphone::getPhraseMetaphone('calça calsa calssa') . PHP_EOL;
 
